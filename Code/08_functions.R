86d141454f7ce9607b1b73d93a44b7e0fcdeccc1
Elenabb3 <elenabetti2002@gmail.com>
2026-04-23 15:01:00 +0200

    Create 08_functions.R

---
+++ Code/08_functions.R
@@ -0,0 +1,14 @@
+#creazione della propria funzione
+
+#es funzione somma
+#function(argomento1,argomento2)
+somma <- function(x,y){
+  z=x+y
+  return(z)
+}
+
+divisione <- function(x,y){
+  z=x/y
+  return(z)
+}
+
