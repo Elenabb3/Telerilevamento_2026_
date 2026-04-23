a04d489568d3f611321f3385926e601b7234e5ce
Elenabb3 <elenabetti2002@gmail.com>
2026-04-23 14:50:51 +0200

    Update 07_classification.R

--- Code/07_classification.R
+++ Code/07_classification.R
@@ -2,6 +2,8 @@
 
 library(terra)
 library(imageRy)
+library(ggplot2)
+library(patchwork)
 
 im.list()
 
@@ -72,6 +74,49 @@ tabout <- data.frame(
 )
       
 tabout
+
+#fare grafici con ggplot. ggplot(argomento in entrata, aes sta per aesthetics)
+#prima parte definisce i dati del grafico, la seconda parte definisce il tipo di grafico/geometria
+#ggplot funziona aggiungendo pezzi col + per aggiungere funzioni, piuttosto che annidare nelle parentesi
+#geom_bar indica la geometria a barre
+#stat indica la statistica - in che senso
+#fill mette il colore di riempimento, non è obbligatorio
+ggplot(tabout, aes(x=class, y=perc92, color=class)) +
+  geom_bar(stat="identity", fill="white")
+    
+ggplot(tabout, aes(x=class, y=perc06, color=class)) +
+  geom_bar(stat="identity", fill="white")
+
+#per visualizzare i plot con patchwork
+p1 <- ggplot(tabout, aes(x=class, y=perc92, color=class)) + geom_bar(stat="identity", fill="white")
+p2 <- ggplot(tabout, aes(x=class, y=perc06, color=class)) + geom_bar(stat="identity", fill="white")
+p1 + p2
+
+#però questi grafici non vanno bene perché hanno due scale diverse. aggiungo ylim
+#voglio anche togliere una delle due legende, perché è ridondante
+
+#poi in realtà in questo caso la legenda non serve proprio, quindi si può fare theme ecc. in entrambi
+#lo sfondo grigio si può tenere (caratteristica di ggplot), ma volendo posso togliera con
+
+p1 <- ggplot(tabout, aes(x=class, y=perc92, color=class)) + 
+  geom_bar(stat="identity", fill="white") +
+  ylim(c(0,100)) +
+  theme(legend.position="none") +
+  theme_minimal()
+
+p2 <- ggplot(tabout, aes(x=class, y=perc06, color=class)) +
+  geom_bar(stat="identity", fill="white") +
+  ylim(c(0,100)) +
+  theme_minimal()
+
+p1 + p2
+
+
+
+
+
+    
+    
 
 
 
